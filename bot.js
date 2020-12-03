8480739b3cf30562872b11539a27155a2584ec34
Joshua Martinez <joshkmartinez@gmail.com>
2020-12-03 09:48:51 -0800

    remove axios

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-require("./server")();
+//require("./server")();
 
 const { Client, MessageEmbed } = require("discord.js");
-const axios = require("axios");
 const Statcord = require("statcord.js");
 const config = require("./config");
 const commands = require("./help");
